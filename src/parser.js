53f6abfc24a0d803214386b4b71eb1f6a136313d
Rob Griffiths <rob@bytespider.eu>
2014-02-04 17:05:35 +0000

    parser speed improvement and simplification

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,7 @@
+'use strict';
+
 var util = require("util");
 var Transform = require("stream").Transform;
-
-var PNMImage = require('./image');
 
 module.exports = Parser;
 util.inherits(Parser, Transform);
@@ -12,128 +12,94 @@ function Parser(options) {
     }
 
     Transform.call(this, options);
-
-    this._writableState.objectMode = false;
-    this._readableState.objectMode = true;
-
     this._parsedMetadata = { type: null, width: 0, height: 0, colors: 0 };
-    this._parsedPixelData = null;
 }
 
+Parser.prototype._headerSent = false;
+
 Parser.prototype._transform = function (chunk, encoding, cb) {
-    console.time('transform');
     var token;
-    if (chunk.length > 0 && this._parsedMetadata.type == null) {
-        console.time('type');
+    var meta = this._parsedMetadata;
+
+    if (chunk.length > 0 && meta.type == null) {
         token = getToken(chunk);
+
         var type = token.bytes.toString();
 
-        if (!Parser.ImageTypes.indexOf(type)) {
+        if (type != 'P4' && type != 'P5' && type != 'P6') {
             this.emit('error', new Error('Image type not supported. Image type: ' + type));
             return;
         }
 
-        this._parsedMetadata.type = type;
-
-        chunk = chunk.slice(token.offset);
-        console.timeEnd('type');
+        meta.type = type;
+        chunk = getChunk(chunk, token.offset);
     }
 
-    if (chunk.length > 0 && this._parsedMetadata.width == 0) {
-        console.time('width');
+    if (chunk.length > 0 && meta.width == 0) {
         token = getToken(chunk);
-        this._parsedMetadata.width = +token.bytes.toString();
 
-        chunk = chunk.slice(token.offset);
-        console.timeEnd('width');
+        meta.width = +token.bytes;
+        chunk = getChunk(chunk, token.offset);
     }
 
-    if (chunk.length > 0 && this._parsedMetadata.height == 0) {
-        console.time('height');
+    if (chunk.length > 0 && meta.height == 0) {
         token = getToken(chunk);
-        this._parsedMetadata.height = +token.bytes.toString();
 
-        chunk = chunk.slice(token.offset);
-        console.timeEnd('height');
+        meta.height = +token.bytes;
+        chunk = getChunk(chunk, token.offset);
     }
 
-    if (chunk.length > 0 && this._parsedMetadata.colors == 0) {
-        console.time('colors');
+    if (chunk.length > 0 && meta.colors == 0) {
         token = getToken(chunk);
-        var maxval = +token.bytes.toString();
+
+        var maxval = +token.bytes;
         if (maxval <= 0 || maxval >= 65536) {
             this.emit('error', new Error('Color max is not within the supported rage for the format: ' + maxval));
             return;
         }
-        this._parsedMetadata.colors = maxval;
 
-        chunk = chunk.slice(token.offset);
-        console.timeEnd('colors');
+        meta.colors = maxval;
+        chunk = getChunk(chunk, token.offset);
     }
 
-    if (chunk.length > 0 && hasMetadata(this._parsedMetadata)) {
-        console.time('data');
+    if (chunk.length > 0 && hasMetadata(meta)) {
         if (this._headerSent == false) {
-            this.emit('header', this._parsedMetadata);
+            this.emit('header', meta);
             this._headerSent = true;
         }
-        // this should be pixel data
-        if (chunk.length) {
-            this.emit('data', chunk);
-        }
 
-        if (null == this._parsedPixelData) {
-            this._parsedPixelData = chunk;
-        } else {
-            this._parsedPixelData = Buffer.concat([this._parsedPixelData, chunk]);
-        }
 
-        if (this._parsedPixelData.length == this._parsedMetadata.width * this._parsedMetadata.height * (this._parsedMetadata.type == 'P6' ? 3 : 1)) {
-            var image = new PNMImage(
-                this._parsedMetadata.width,
-                this._parsedMetadata.height,
-                this._parsedMetadata.colors,
-                this._parsedPixelData
-            );
-
-            console.timeEnd('transform');
-            return cb(null, image);
-        }
-        console.timeEnd('data');
+        this.push(chunk);
     }
 
-    cb();
-    console.timeEnd('transform');
+    return cb();
 };
-
-Parser.prototype._flush = function (cb) {
-    cb();
-};
-
-// only the binary formats are supported
-Parser.ImageTypes = [ 'P4', 'P5', 'P6' ];
 
 function getToken(chunk) {
-    console.time('token');
-    var i = 0;
-    while(!isWhitespace(chunk[i])) { i += 1 }
+    var i = 0, offset;
 
-    if (i > chunk.length) {
+    // find the whitespace
+    while(!isWhitespace(chunk[i])) { i += 1 }
+    
+    // keep looking until no more whitespace
+    offset = i + 1;
+    while(isWhitespace(chunk[offset])) { offset += 1 }
+
+    if (i > chunk.length || offset > chunk.length) {
         return false; // couldn't determine a token, ran out of bytes
     }
 
-    console.timeEnd('token');
-    return {offset: i + 1, bytes: chunk.slice(0, i)};
+    return {offset: offset, bytes: chunk.slice(0, i)};
+}
+
+function getChunk(chunk, offset, length) {
+    return chunk.slice(offset, length);
 }
 
 function hasMetadata(meta) {
     return meta.type != null && meta.type != '' && meta.width > 0 && meta.height > 0 && meta.colors > 0;
 }
 
-function parseMetadataType(chunk) {
-
-}
-
 function isWhitespace(byte) {
-    return [0x20, 0x09, 0x0a, 0x0b, 0x0c, 0x0d].indexOf(byte) !== -1;
+    return (byte >= 9 && byte <= 13) || byte == 32;
 }
